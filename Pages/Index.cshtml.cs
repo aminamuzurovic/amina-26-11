19a957843e28826508180a13a4ef60d474ebc40d
Amina <aminamuzurovic43@gmail.com>
2019-11-26 16:04:21 +0100

    Added folders from amina-26-11-repo

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace amina_26_11_repo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
